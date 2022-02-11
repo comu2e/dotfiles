af068503ce20e93434ab202776b215644464d5c3
comu2e <comu2e@gmail.com>
2022-02-11 16:30:06 +0900

    fix :comment

--- install.sh
+++ install.sh
@@ -60,11 +60,11 @@ log "asdf already finished"
 stow -v -d ~/dotfiles/packages/termial/ -t ~ alacritty fish omf starship tmux
 stow -v -d ~/dotfiles/packages/versioning -t ~ git-templates
 stow -v -d ~/dotfiles/packages/editor -t ~ nvim
-# stow -v -d ~/dotfiles/packages/runtime -t ~ runtime
-# stow -v -d ~/dotfiles/packages/wm -t ~ limelight yabai
+ stow -v -d ~/dotfiles/packages/runtime -t ~ runtime
+stow -v -d ~/dotfiles/packages/wm -t ~ limelight yabai
 stow -v -d ~/dotfiles/packages/keybindings -t ~ karabiner
-# ln -sf "~/.config/yabai/yabairc" "~/.yabairc"
-# ln -sf "~/.config/yabai/skhdrc" "~/.skhdrc"
+ln -sf "~/.config/yabai/yabairc" "~/.yabairc"
+ln -sf "~/.config/yabai/skhdrc" "~/.skhdrc"
 
 is_runtime_versions_changed () {
     plugin="$1"
